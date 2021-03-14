2eb4a85c0532de1c6fabffef8197db709f47596b
Maximilian Liebsch <43824628+MaLIE2018@users.noreply.github.com>
2021-03-14 16:30:57 +0100

    Exercise 6 to 13 well done!

--- index.js
+++ index.js
@@ -95,10 +95,116 @@ function animate_string(id) {
 
 }
 
+//Exercise 6 Sunday, 3/14/2021
 
-//Excecution
-rotateString()
-    //areaOfTriangle()
-    //printCurrentDate()
-changeContent(outputDayaTime(), "e1");
-//printContent()
+// 6. Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar. 
+
+/* Alle durch 100 teilbaren Schaltjahre fallen nämlich aus, also zum Beispiel im Jahr 1900 und dann wieder 2100. 
+Weil das aber auch nicht ganz stimmen würde, gibt es auch von dieser Regel wieder eine Ausnahme: 
+Alle Schaltjahre, die durch 400 teilbar sind, bleiben Schaltjahre, also 1600, 2000 und dann wieder 2400.
+Das Jahr 2000 war also ein Schaltjahr, denn 2000 ist durch 100 und 400 teilbar. 
+Hingegen wird 2100 kein Schaltjahr sein, weil 2100 nicht durch 400 teilbar ist. 
+Alle, die also am 29. Februar Geburtstag haben, legen sich am besten gleich einen Taschenrechner zu, 
+damit sie ihren nächsten Geburtstag nicht verpassen. :-) */
+
+const isLeapYear = (year) => {
+    return (year % 100 === 0) ? year % 400 === 0 : year % 4 === 0
+}
+
+
+// 7. Write a JavaScript program to find 1st January is being a Sunday between 2014 and 2050.
+
+
+const firstJanSundayIn46years = () => {
+    let startDate = new Date(Date.UTC(2014, 0, 1))
+    let endDate = new Date(Date.UTC(2050, 0, 1))
+    let years = endDate.getFullYear() - startDate.getFullYear()
+
+    for (let i = 1; i <= years; i++) {
+        if (startDate.getDay() === 0) console.log('1st Jan is Sunday:', startDate.getFullYear())
+        startDate = new Date(Date.UTC(startDate.getFullYear() + 1, 0, 1))
+    }
+}
+
+/* 8. Write a JavaScript program where the program takes a random integer between 1 to 10, 
+the user is then prompted to input a guess number. If the user input matches with guess number, 
+the program will display a message "Good Work" otherwise display a message "Not matched"
+ */
+
+const guessGame = (num) => {
+    console.log('num:', num)
+    let random = Math.floor((Math.random() * 10) + 1)
+    if (parseInt(num) === random) {
+
+        alert("Good Work")
+    } else {
+        alert(`not matched the number was ${random}`)
+    }
+}
+
+/* 9. Write a JavaScript program to calculate days left until next Christmas. */
+
+const whenIsChristmas = () => {
+    let today = new Date()
+    let christmas = new Date(Date.UTC(today.getFullYear(), 11, 24))
+
+    if (today.getMonth === 11 && today.getDay === 24) {
+        christmas.setFullYear(christmas.setFullYear() + 1)
+    }
+
+    return Math.floor((christmas - today) / (1000 * 3600 * 24))
+}
+
+
+/* 10. Write a JavaScript program to calculate multiplication and division of two numbers (input from user).  Go to the editor
+Sample form :  */
+
+
+const multiply = () => {
+    n1 = document.getElementById('num1').value
+    n2 = document.getElementById('num2').value
+    document.getElementById('result').innerHTML = n1 * n2
+}
+
+const divide = () => {
+        n1 = document.getElementById('num1').value
+        n2 = document.getElementById('num2').value
+        document.getElementById('result').innerHTML = n1 / n2
+    }
+    /* 11. Write a JavaScript program to convert temperatures to and from Celsius, Fahrenheit.  
+    [ Formula : c/5 = (f-32)/9 [ where c = temperature in Celsius and f = temperature in Fahrenheit ]
+    Expected Output :
+    60°C is 140 °F
+    45°F is 7.222222222222222°C  */
+
+const tempConverter = (temp, isCelsius) => {
+    if (isCelsius) {
+        c = temp
+        f = (c / 5) * 9 + 32
+        return `${c}\xB0C is ${f}\xB0F`
+    } else {
+        f = temp
+        c = ((f - 32) / 9) * 5
+        return `${f}\xB0F is ${c}\xB0C`
+    }
+}
+
+/* 12. Write a JavaScript program to get the website URL (loading page). */
+
+const getWebsiteURL = () => {
+    document.getElementById("URL").innerHTML = "URL of this page " + window.location.href
+}
+
+getWebsiteURL()
+
+/* 13. Write a JavaScript exercise to create a variable using a user-defined name */
+
+const userdefinedVariable = () => {
+    let n = 102
+    let userVar = document.getElementById('userVar').value
+    this[userVar] = n
+
+    document.getElementById('result13').innerHTML = this[userVar]
+    console.log(this[userVar])
+
+}
